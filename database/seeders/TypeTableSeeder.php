0c363025e80853d337f44ade1cc922731248a3d8
raffaelecapaldo <raffaelecpl@gmail.com>
2023-05-26 18:46:24 +0200

    Seed now takes data from the csv

--- database/seeders/TypeTableSeeder.php
+++ database/seeders/TypeTableSeeder.php
@@ -16,13 +16,44 @@ class TypeTableSeeder extends Seeder
      *
      * @return void
      */
+
+
     public function run(Faker $faker)
     {
-        for ($i = 0; $i < 12; $i++) {
-            $newType = new Type();
-            $newType->name = $faker->word();
-            $newType->description = $faker->sentence();
-            $newType->save();
+        function getCsvData(string $path)
+        {
+            // Crea un'array vuota
+            $data = [];
+
+            // Apri il file stream in lettura
+            $file_stream = fopen($path, 'r');
+
+            // Verifica se il file non é stato aperto e mostra errore
+            if ($file_stream === false) {
+                exit('Cannot open the file ' . $path);
+            }
+
+            // leggi le singole righe del csv fino alla fine
+            // fgetcsv restituisce o l'array o false
+            while (($row = fgetcsv($file_stream)) !== false) {
+                // salva ciascuna riga come elemento dell'array
+                $data[] = $row;
+            }
+
+            // chiudi il file
+            fclose($file_stream);
+            // restituisci i dati
+            return $data;
+        }
+        $types = getCsvData('resources\csv\classes.csv');
+
+        foreach ($types as $index => $row) {
+            if ($index !== 0) {
+                $type = new Type();
+                $type->name = $row[0];
+                $type->description = $row[1];
+                $type->save();
+            }
         }
     }
 }
