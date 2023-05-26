dd566ecd9f60712e48fc848d6a12be79c1563fc2
filecc <fil.gia@live.it>
2023-05-26 16:26:34 +0200

    fix typo line 9 itemstable

--- database/seeders/ItemsTableSeeder.php
+++ database/seeders/ItemsTableSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
-use App\Models\Items;
+use App\Models\Item;
 
 class ItemsTableSeeder extends Seeder
 {
