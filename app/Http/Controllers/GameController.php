f554c2866222dfba02075bec7f457b79bf1807df
Lip-7 <zhighofficial@gmail.com>
2023-05-26 17:12:41 +0200

    minor fixes on routes and characters.index

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 class GameController extends Controller
 {
     public function index(){
+        return view('home');
+    }
+    public function characters(){
         return view('characters.index');
+    }
+    public function show(){
+        return view('characters.show');
     }
 }
--- resources/views/characters/index.blade.php
+++ resources/views/characters/index.blade.php
@@ -5,8 +5,11 @@
             <div class="card text-dark" style="width: 18rem;">
                 <img src="{{ Vite::asset('resources/img/download.jpg') }}" class="card-img-top" alt="...">
                 <div class="card-body">
-                  <h5 class="card-title">Giampapa</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <h4 class="card-title">Giampapa</h4>
+                  <div class="type">
+                    <p>Class: <span class="mb-1 fw-bold">Warrior</span></p>
+
+                  </div>
                   <a href="#" class="btn btn-primary">More infos</a>
                 </div>
               </div>
