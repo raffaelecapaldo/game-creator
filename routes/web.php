4181950ec9f3e229b35cacdc23049e3d97a0bd4e
raffaelecapaldo <raffaelecpl@gmail.com>
2023-06-06 16:33:58 +0200

    Added require auth.php

--- routes/web.php
+++ routes/web.php
@@ -22,13 +22,14 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+/* Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+}); */
 
 Route::get('/', [GameController::class, 'index'] )->name('home');
 Route::get('/characters', [GameController::class, 'characters'] )->name('characters.index');
 Route::get('/characters/{id}', [GameController::class, 'show'] )->name('characters.show');
 
+require __DIR__ . '/auth.php';
