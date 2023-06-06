2257c86598c1706660870080ed82bddd0d08582e
Samuel Panicucci <samuelp88@gmail.com>
2023-06-06 16:20:34 +0200

    Aggiunta la creazione dei Character nel Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,5 +24,9 @@ class DatabaseSeeder extends Seeder
         $this->call([
             TypeTableSeeder::class,
         ]);
+
+        $this->call([
+            CharactersTableSeeder::class,
+        ]);
     }
 }
