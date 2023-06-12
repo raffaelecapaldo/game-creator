9255e06e1cee73bb4aed582f34dee52ae26707d6
raffaelecapaldo <raffaelecpl@gmail.com>
2023-06-12 16:41:24 +0200

    Added attach and synch for items

--- app/Models/Character.php
+++ app/Models/Character.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Character extends Model
 {
     use HasFactory;
-    protected $guarded = [];
+    protected $guarded = ['items'];
 
     public function type()
     {
