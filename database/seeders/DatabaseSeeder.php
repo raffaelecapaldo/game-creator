0d2e49343385fd1737189a97e4b1e53ae0fccb01
raffaelecapaldo <raffaelecpl@gmail.com>
2023-06-12 15:58:42 +0200

    Added seeder (Items) - Created helper file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,5 +28,9 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CharactersTableSeeder::class,
         ]);
+
+        $this->call([
+            ItemsTableSeeder::class,
+        ]);
     }
 }
